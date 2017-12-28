0d80a065d822496871381620e1b1aeb8c8cb8134
wurongqiu <wurongqiu@kaike.la>
2017-12-28 15:44:44 +0800

    integrate online

--- app/src/main/java/com/wcamera/qr/qrcamera/MainActivity.java
+++ app/src/main/java/com/wcamera/qr/qrcamera/MainActivity.java
@@ -1,7 +1,12 @@
 package com.wcamera.qr.qrcamera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+//import com.wcamera.qr.qrcameralib.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent() ;
+                intent.setClass(MainActivity.this, CaptureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
